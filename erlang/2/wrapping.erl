bb0864127efdd0e2993acf5712d74701597d5cba
Huw Martin <huw.martin@gmail.com>
2016-06-13 00:32:58 +0100

    Part two of wrapping exercise
    Extracts logic for splitting string into list of lists into function;
    extracts summing logic into function; adds logic to calculate ribbon
    length.

--- erlang/2/wrapping.erl
+++ erlang/2/wrapping.erl
@@ -1,18 +1,28 @@
 -module(wrapping).
--export([calculate/1, erm/1]).
+-export([paper/1, ribbon/1]).
 
-erm(String) ->
-  lists:foldl(
-    fun(Curr, Sum) ->
-      Sum + calculate(erlang:list_to_tuple(Curr))
-    end,
-  0, lists:map(fun(Str) -> string:tokens(Str, "x") end, string:tokens(String, "\n"))).
+paper(String) ->
+  calculate_amount(fun calculate_paper/1, String).
 
-calculate({Length, Width, Height})  ->
-  L = string:to_integer(Length),
-  W = string:to_integer(Width),
-  H = string:to_integer(Height),
-  erlang:display(L),
-  erlang:display(W),
-  erlang:display(H),
-  (2*(L*W)) + (2*(W*H)) + (2*(H*L)) + erlang:min(erlang:min((2*(L*W)), (2*(W*H)), (2*(H*L)))).
+ribbon(String) ->
+  calculate_amount(fun calculate_ribbon/1, String).
+
+calculate_amount(Fn, String) ->
+  lists:foldl(fun(Curr, Sum) -> Sum + Fn(Curr) end, 0, parse_string(String)).
+
+parse_string(String) ->
+  lists:map(fun(Str) -> string:tokens(Str, "x") end, string:tokens(String, "\n")).
+
+calculate_paper([Length, Width, Height])  ->
+  {L, _} = string:to_integer(Length),
+  {W, _} = string:to_integer(Width),
+  {H, _} = string:to_integer(Height),
+  2*L*W + 2*W*H + 2*H*L + lists:min([L*W, W*H, H*L]).
+
+calculate_ribbon([Length, Width, Height])  ->
+  {L, _} = string:to_integer(Length),
+  {W, _} = string:to_integer(Width),
+  {H, _} = string:to_integer(Height),
+  X = lists:min([L, W, H]),
+  Y = lists:min(lists:delete(X, [L, W, H])),
+  X*2 + Y*2 + L*W*H.
--- erlang/2/wrapping_tests.erl
+++ erlang/2/wrapping_tests.erl
@@ -7,7 +7,13 @@
 -include_lib("eunit/include/eunit.hrl").
 
 wrapping_test() ->
-  ?assertEqual(58, wrapping:calculate(2, 3, 4)).
+  ?assertEqual(58, wrapping:paper("2x3x4")).
 
 wrapping_test_next() ->
-  ?assertEqual(43, wrapping:calculate(1, 1, 10)).
+  ?assertEqual(43, wrapping:paper("1x1x10")).
+
+ribbon_test() ->
+  ?assertEqual(34, wrapping:ribbon("2x3x4")).
+
+ribbon_test_next() ->
+  ?assertEqual(14, wrapping:ribbon("1x1x10")).
